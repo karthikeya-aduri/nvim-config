7c195cc57c64ad70d034ed993fbf1e9e86db2cfd
karthikeya-aduri <131266273+karthikeya-aduri@users.noreply.github.com>
2024-07-30 23:52:05 +0530

    Added some basic settings of neovim.

---
+++ lua/config/settings.lua
@@ -0,0 +1,23 @@
+local o = vim.opt
+
+o.nu = true
+o.relativenumber = true
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+
+o.smartindent = true
+
+o.wrap = false
+
+o.swapfile = false
+o.backup = false
+
+o.hlsearch = false
+o.incsearch = true
+
+o.scrolloff = 8
+
+o.showmode = false
