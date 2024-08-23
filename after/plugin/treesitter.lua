f059811764ac9e5787d2722bb6b3bf6e90bb109f
karthikeya-aduri <131266273+karthikeya-aduri@users.noreply.github.com>
2024-08-23 22:16:54 +0530

    Updated ensure_installed in treesitter.lua

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,13 +1,27 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-      "html", "css", "javascript", "typescript", "json",
-      "c", "cpp", "cmake", "sql", "java", "python", "lua", "markdown", "vim", "vimdoc", "query"
-  },
-  sync_install = false,
-  auto_install = true,
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"lua",
+		"python",
+		"c",
+		"cpp",
+		"html",
+		"css",
+		"javascript",
+		"markdown",
+		"json",
+		"sql",
+		"gitcommit",
+		"gitignore",
+		"cmake",
+		"vim",
+		"vimdoc",
+		"query",
+	},
+	sync_install = false,
+	auto_install = true,
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+})
